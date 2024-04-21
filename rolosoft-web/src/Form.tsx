74acfff77c883e6fc4694f34048f8eb236a1fba3
Fernanda De León <fernandadeleon@Fernandas-MacBook-Pro.local>
2024-04-21 02:21:51 -0600

    Add upload images

--- rolosoft-web/src/Form.tsx
+++ rolosoft-web/src/Form.tsx
@@ -1,17 +1,9 @@
 import React from 'react';
-import {
-  Button,
-  Cascader,
-  DatePicker,
-  Form,
-  Input,
-  InputNumber,
-  Mentions,
-  Select,
-  TreeSelect,
-} from 'antd';
+import { Button, DatePicker, Form, Input, InputNumber, Select, Upload } from 'antd';
+import { InboxOutlined } from '@ant-design/icons';
 
-const { RangePicker } = DatePicker;
+const { Dragger } = Upload;
+const { Option } = Select;
 
 const formItemLayout = {
   labelCol: {
@@ -26,46 +18,137 @@ const formItemLayout = {
 
 const App: React.FC = () => (
   <Form {...formItemLayout} variant="filled" style={{ maxWidth: 600 }}>
-    <Form.Item label="Nombre(s)" name="Input" rules={[{ required: true, message: 'Please input!' }]}>
-      <Input />
-    </Form.Item>
-
-    <Form.Item label="Apellido(s)" name="Input" rules={[{ required: true, message: 'Please input!' }]}>
+    <Form.Item
+      label="Nombre Completo"
+      name="fullName"
+      rules={[{ required: true, message: 'Por favor ingresa tu nombre completo' }]}
+    >
       <Input />
     </Form.Item>
 
     <Form.Item
       label="Fecha de nacimiento"
-      name="DatePicker"
-      rules={[{ required: true, message: 'Please input!' }]}
+      name="dateOfBirth"
+      rules={[{ required: true, message: 'Por favor selecciona tu fecha de nacimiento' }]}
     >
       <DatePicker />
     </Form.Item>
 
-    <Form.Item label="Domicilio" name="Input" rules={[{ required: true, message: 'Please input!' }]}>
+    <Form.Item
+      label="Escuela"
+      name="school"
+      rules={[{ required: true, message: 'Por favor selecciona tu escuela' }]}
+    >
+      <Select>
+        <Option value="escuela1">Escuela 1</Option>
+        <Option value="escuela2">Escuela 2</Option>
+      </Select>
+    </Form.Item>
+
+    <Form.Item
+      label="Grado"
+      name="grade"
+      rules={[{ required: true, message: 'Por favor selecciona tu grado' }]}
+    >
+      <Select>
+        <Option value="grado1">Grado 1</Option>
+        <Option value="grado2">Grado 2</Option>
+      </Select>
+    </Form.Item>
+
+    <Form.Item
+      label="Domicilio"
+      name="address"
+      rules={[{ required: true, message: 'Por favor ingresa tu domicilio' }]}
+    >
       <Input />
     </Form.Item>
 
-    <Form.Item label="CURP" name="Input" rules={[{ required: true, message: 'Please input!' }]}>
+    <Form.Item
+      label="CURP"
+      name="curp"
+      rules={[{ required: true, message: 'Por favor ingresa tu CURP' }]}
+    >
       <Input />
     </Form.Item>
 
-    <Form.Item label="Nombre del padre, madre o tutor" name="Input" rules={[{ required: true, message: 'Please input!' }]}>
+    <Form.Item
+      label="Nombre del padre, madre o tutor"
+      name="parentName"
+      rules={[{ required: true, message: 'Por favor ingresa el nombre del padre, madre o tutor' }]}
+    >
       <Input />
     </Form.Item>
 
-    <Form.Item label="Select" name="Select" rules={[{ required: true, message: 'Please input!' }]}>
-      <Select />
+    <Form.Item
+      label="Teléfono de contacto"
+      name="phone"
+      rules={[{ required: true, message: 'Por favor ingresa tu teléfono de contacto' }]}
+    >
+      <Input />
     </Form.Item>
 
-    
+    <Form.Item
+      label="Correo electrónico"
+      name="email"
+      rules={[
+        { required: true, message: 'Por favor ingresa tu correo electrónico' },
+        { type: 'email', message: 'Por favor ingresa un correo electrónico válido' },
+      ]}
+    >
+      <Input />
+    </Form.Item>
+
+    <Form.Item
+      label="No. afiliación IMSS"
+      name="imssNumber"
+      rules={[
+        { required: true, message: 'Por favor ingresa tu número de afiliación del IMSS' },
+        { type: 'string', len: 11, message: 'El número de afiliación IMSS debe tener 11 dígitos' },
+      ]}
+    >
+      <Input />
+    </Form.Item>
+
+    <Form.Item label="Añadir una fotografía personalizada">
+      <Form.Item name="photo" valuePropName="fileList" getValueFromEvent={normFile} noStyle>
+        <Upload.Dragger name="photo" multiple={false} accept="image/*">
+          <p className="ant-upload-drag-icon">
+            <InboxOutlined />
+          </p>
+          <p className="ant-upload-text">Haz clic o arrastra una imagen aquí para subirla</p>
+          <p className="ant-upload-hint">Imagen no mayor a 1MB</p>
+        </Upload.Dragger>
+      </Form.Item>
+    </Form.Item>
+
+    <Form.Item
+      label="Documentación física"
+      name="physicalDocumentation"
+      rules={[{ required: true, message: 'Por favor sube la documentación física requerida' }]}
+    >
+      <Dragger name="physicalDocumentation" multiple={true}>
+        <p className="ant-upload-drag-icon">
+          <InboxOutlined />
+        </p>
+        <p className="ant-upload-text">Haz clic o arrastra los archivos aquí para subirlos</p>
+        <p className="ant-upload-hint">Carta deslinde de responsabilidades, CURP del participante, INE del padre, madre o tutor</p>
+      </Dragger>
+    </Form.Item>
 
     <Form.Item wrapperCol={{ offset: 6, span: 16 }}>
       <Button type="primary" htmlType="submit">
-        Submit
+        Enviar
       </Button>
     </Form.Item>
   </Form>
 );
+
+const normFile = (e: { fileList: any }) => {
+  if (Array.isArray(e)) {
+    return e;
+  }
+  return e && e.fileList;
+};
 
 export default App;
